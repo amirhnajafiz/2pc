60417af3042204ebe957f87f032ea1323b6f7d5f
amirhnajafiz <najafizadeh21@gmail.com>
2024-11-17 18:20:25 -0500

    add: cluster main

--- cluster/main.go
+++ cluster/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/F24-CSE535/2pc/cluster/cmd"
+)
+
+func main() {
+	args := os.Args
+	if len(args) < 3 {
+		panic("at least one argument is needed")
+	}
+
+	// create a new cluster manager
+	cm := cmd.Cluster{
+		ClusterName: args[1],
+		ConfigPath:  args[2],
+	}
+
+	// start the cluster manager
+	if err := cm.Main(); err != nil {
+		panic(err)
+	}
 }
