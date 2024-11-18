b44a70b956eae1f060565fc2eadba79411e525d9
amirhnajafiz <najafizadeh21@gmail.com>
2024-11-17 22:06:48 -0500

    update: scripts

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# running ./main <ip-tables> <mongo-uri> <database>
 ./main ../configs/iptable.txt $(cat ../configs/mongo.txt) global
---
+++ scripts/cluster.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# running ./main <cluster-name> <config-path> <index>
+./main C1 ../configs/clusters.yaml 1
