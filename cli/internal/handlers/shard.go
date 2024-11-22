fbeaafb9e2e0973ee80a8a12a5bf39699d5a34cf
amirhnajafiz <najafizadeh21@gmail.com>
2024-11-22 16:37:26 -0500

    refactor: shards load

--- cli/internal/handlers/shard.go
+++ cli/internal/handlers/shard.go
@@ -20,22 +20,18 @@ func (c *ShardHandler) GetName() string {
 
 func (c *ShardHandler) Execute(argc int, args []string) error {
 	// four arguments are needed
-	if argc != 4 {
+	if argc != 3 {
 		return fmt.Errorf("mismatch input arguments: count%d expected 4", argc)
 	}
 
 	// open database connection
-	db, err := database.NewDatabase(args[2], args[3])
+	db, err := database.NewDatabase(args[1], args[2])
 	if err != nil {
 		return fmt.Errorf("open database failed: %v", err)
 	}
 
-	// parse input csv files (args[0] is datastore path and args[1] is shards path)
-	datastore, err := utils.CSVParseDatastoreFile(args[0])
-	if err != nil {
-		return fmt.Errorf("failed to parse datastore: %v", err)
-	}
-	shards, err := utils.CSVParseShardsFile(args[1])
+	// parse input csv files (args[0] is the shards path)
+	shards, err := utils.CSVParseShardsFile(args[0])
 	if err != nil {
 		return fmt.Errorf("failed to parse shards: %v", err)
 	}
@@ -60,19 +56,12 @@ func (c *ShardHandler) Execute(argc int, args []string) error {
 			return fmt.Errorf("failed to parse range %s: %v", shard[0], err)
 		}
 
-		list = append(list, &tmp)
-	}
-
-	// loop over datastore to put each item into its shard
-	for key, value := range datastore {
-		index, _ := strconv.Atoi(strings.Replace(key, "S", "", -1)) // remove the S from client name
-
-		for _, item := range list {
-			// client index should be in the range of shard
-			if item.StartId <= index && index < item.EndId {
-				item.Clients[key] = value
-			}
+		// create clients
+		for i := tmp.StartId; i <= tmp.EndId; i++ {
+			tmp.Clients[fmt.Sprintf("%d", i)] = 10
 		}
+
+		list = append(list, &tmp)
 	}
 
 	// print some metadata
@@ -87,7 +76,7 @@ func (c *ShardHandler) Execute(argc int, args []string) error {
 			shard.Cluster,
 			len(shard.Clients),
 			shard.StartId,
-			shard.EndId-1,
+			shard.EndId,
 		)
 	}
 
